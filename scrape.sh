9e7c0953460acdeadb6fd91e626ae60cf59f620e
Carlos Martinez <cmartinez@ciencias.unam.mx>
2025-11-24 19:07:33 -0600

    Startingout

--- scrape.sh
+++ scrape.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-USERS= "vickywolff" "juliolealortiz" "edificiosmayas" "santos_tuz" "mercibir" "el socotroco" "lasmacucasoficial" "tilasesto"
+USERS=("vickywolff" "juliolealortiz" "edificiosmayas" "santos_tuz" "mercibir" "el socotroco" "lasmacucasoficial" "tilasesto")
+
+instaloader --no-compress-json --comments +args.txt profile "${USERS[@]}"
+
